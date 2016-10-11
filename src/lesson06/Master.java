3695685928d9aebe0daef6882fd3c11e63c57fdc
Mac <ukitomato@gmail.com>
2016-10-11 22:17:28 +0900

    10/11

---
+++ src/lesson06/Master.java
@@ -0,0 +1,11 @@
+package lesson06;
+
+import java.util.ArrayList;
+
+public class Master {
+    private ArrayList players_ = new ArrayList();
+
+    public void prepareGame(Hand cards) {
+        Sy
+    }
+}
